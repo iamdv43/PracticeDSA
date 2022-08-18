94eb4453fd6af63d6ea0ca4cadd5af5db7707fac
iamdv43 <iamdv43@gmail.com>
2022-08-17 21:06:50 -0300

    restoreIP

---
+++ restorIp.py
@@ -0,0 +1,21 @@
+def restoreIp(s):
+    ret = []
+    r = [1, 2, 3]
+    for a in r:
+        for b in r:
+            for c in r:
+                for d in r:
+                    if a + b + c + d != len(s):
+                        continue
+                    nums_str = [s[:a], s[a:a+b], s[a+b:a+b+c], s[a+b+c:]]
+                    nums_int = [int(x) for x in nums_str if int(x)<256]
+                    if len(nums_int) != 4:
+                        continue
+                    nums_str_without_prefix = [str(x) for x in nums_int]
+                    if nums_str != nums_str_without_prefix:
+                        continue
+                    ret.append('.'.join(nums_str))
+
+    return ret
+
+print("Ans: ", restoreIp("25525511135"))
